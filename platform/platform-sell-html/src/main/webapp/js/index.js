90e8c71ff7ed2144cb8d76512331271a2f3e182e
YuLina <YuLina@AFOC-1604092017>
2016-07-29 16:40:18 +0800

    画主页面

---
+++ platform/platform-sell-html/src/main/webapp/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+	$("#slidershow").carousel({
+		interval : 2000
+	});
+	$("#slidershow a.left").click(function() {
+		$(".carousel").carousel("prev");
+	});
+	$("#slidershow a.right").click(function() {
+		$(".carousel").carousel("next");
+	});
+	$("#button1").click(function(){
+        $("#slidershow").carousel("1");
+     });
+	$(".start-slide").click(function(){
+        $("#slidershow").carousel('cycle');
+     });
+     // 停止轮播
+     $(".pause-slide").click(function(){
+        $("#slidershow").carousel('pause');
+     });
+     // 循环轮播到上一个项目
+     $(".prev-slide").click(function(){
+        $("#slidershow").carousel('prev');
+     });
+     // 循环轮播到下一个项目
+     $(".next-slide").click(function(){
+        $("#slidershow").carousel('next');
+     });
+     // 循环轮播到某个特定的帧 
+     $(".slide-one").click(function(){
+        $("#slidershow").carousel(0);
+     });
+     $(".slide-two").click(function(){
+        $("#slidershow").carousel(1);
+     });
+     $(".slide-three").click(function(){
+        $("#slidershow").carousel(2);
+     });
+     $(".slide-four").click(function(){
+         $("#slidershow").carousel(3);
+      });
+     $(".slide-five").click(function(){
+         $("#slidershow").carousel(4);
+      });
+     $(".slide-six").click(function(){
+         $("#slidershow").carousel(5);
+      });
+});
